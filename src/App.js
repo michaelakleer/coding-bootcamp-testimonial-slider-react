647d71678705872dfc74d30f9e2448fa06199d5c
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-01-30 17:40:01 -0600

    Added data and imported images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
+import React from "react";
+import Tanya from "./images/image-tanya.jpg";
+import John from "./images/image-john.jpg";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <p>
-        {" "}
-        “ I’ve been interested in coding for a while but never taken the jump,
-        until now. I couldn’t recommend this course enough. I’m now in the job
-        of my dreams and so excited about the future. ” Tanya Sinclair UX
-        Engineer
-      </p>
-
-      <p>
-        “ If you want to lay the best foundation possible I’d recommend taking
-        this course. The depth the instructors go into is incredible. I now feel
-        so confident about starting up as a professional developer. ” John
-        Tarkpor Junior Front-end Developer
-      </p>
-
+      <section className="container">
+        <div className="slider">
+          <div className="slideOne">
+            <div className="testimonial">
+              <p className="text">
+                “ I’ve been interested in coding for a while but never taken the
+                jump, until now. I couldn’t recommend this course enough. I’m
+                now in the job of my dreams and so excited about the future. ”
+              </p>
+              <p className="name">
+                Tanya Sinclair <span>UX Engineer</span>{" "}
+              </p>
+            </div>
+            <div className="photo">
+              <img src={Tanya} alt="Portrait of Tanya" className="portrait" />
+            </div>
+          </div>
+          <div className="slideTwo">
+            <div className="testimonial">
+              <p className="text">
+                “ If you want to lay the best foundation possible I’d recommend
+                taking this course. The depth the instructors go into is
+                incredible. I now feel so confident about starting up as a
+                professional developer. ”
+              </p>
+              <p className="name">
+                John Tarkpor <span>Junior Front-end Developer</span>{" "}
+              </p>
+            </div>
+            <div className="photo">
+              <img src={John} alt="Portrait of John" className="portrait" />
+            </div>
+          </div>
+        </div>
+      </section>
       <div className="attribution">
         Challenge by{" "}
         <a
