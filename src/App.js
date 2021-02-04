ff3c9272fb3336af68f337e7b82d3acd8a24f461
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-02-03 18:45:17 -0600

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -6,44 +6,41 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <main>
+      <div className="slider">
         <section className="container">
-          <div className="slider">
-            <div className="slideOne">
-              <div className="testimonial">
-                <p className="text">
-                  “ I’ve been interested in coding for a while but never taken
-                  the jump, until now. I couldn’t recommend this course enough.
-                  I’m now in the job of my dreams and so excited about the
-                  future. ”
-                </p>
-                <p className="name">
-                  Tanya Sinclair <span>UX Engineer</span>{" "}
-                </p>
-              </div>
-              <div className="photo">
-                <img src={Tanya} alt="Portrait of Tanya" className="portrait" />
-              </div>
+          <div className="slideOne">
+            <div className="testimonial">
+              <p className="text">
+                “ I’ve been interested in coding for a while but never taken the
+                jump, until now. I couldn’t recommend this course enough. I’m
+                now in the job of my dreams and so excited about the future. ”
+              </p>
+              <p className="name">
+                Tanya Sinclair <span>UX Engineer</span>{" "}
+              </p>
             </div>
-            <div className="slideTwo">
-              <div className="testimonial">
-                <p className="text">
-                  “ If you want to lay the best foundation possible I’d
-                  recommend taking this course. The depth the instructors go
-                  into is incredible. I now feel so confident about starting up
-                  as a professional developer. ”
-                </p>
-                <p className="name">
-                  John Tarkpor <span>Junior Front-end Developer</span>{" "}
-                </p>
-              </div>
-              <div className="photo">
-                <img src={John} alt="Portrait of John" className="portrait" />
-              </div>
+            <div className="photo">
+              <img src={Tanya} alt="Portrait of Tanya" className="portrait" />
+            </div>
+          </div>
+          <div className="slideTwo">
+            <div className="testimonial">
+              <p className="text">
+                “ If you want to lay the best foundation possible I’d recommend
+                taking this course. The depth the instructors go into is
+                incredible. I now feel so confident about starting up as a
+                professional developer. ”
+              </p>
+              <p className="name">
+                John Tarkpor <span>Junior Front-end Developer</span>{" "}
+              </p>
+            </div>
+            <div className="photo">
+              <img src={John} alt="Portrait of John" className="portrait" />
             </div>
           </div>
         </section>
-      </main>
+      </div>
     </div>
   );
 }
